375f897cf10ce967e0079c25a5c7b4f74a5db83e
Krody Robert <robi@presslabs.com>
2015-05-25 13:31:39 +0300

    Added possible replacement suggestion for the keyboard

--- pytris/keyboard.py
+++ pytris/keyboard.py
@@ -3,6 +3,9 @@ import tty
 import termios
 
 
+# TODO maybe check
+# http://stackoverflow.com/questions/1054380/how-can-you-read-keystrokes-when-the-python-program-isnt-in-the-foreground
+# evdev
 def get_key():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
